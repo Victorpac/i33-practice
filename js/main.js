4a7fb4f8a09fab92f0769b191f6ca003fdf24525
Victor <victorpac2019@gmail.com>
2024-01-04 14:29:37 +0100

    Made burger menu.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const openMenuButton = document.querySelector('#openMenuButton');
+const searchForm = document.querySelector("#navigationSearchForm")
 
 openMenuButton.addEventListener("click", openMenu);
 
 function openMenu(event) {
   const headerEl = document.querySelector("#header");
   let isOpenMenuButtonActive = openMenuButton.getAttribute("data-is-active") === "true";
+  searchForm.setAttribute("data-is-active", !isOpenMenuButtonActive);
   openMenuButton.setAttribute("data-is-active", !isOpenMenuButtonActive);
   headerEl.setAttribute("data-is-active", !isOpenMenuButtonActive);
 }
