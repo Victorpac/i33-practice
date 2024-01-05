1aeeec9f9b9d2f0fb1d4ef3159ca2df14ba5bbd0
Victor <victorpac2019@gmail.com>
2024-01-05 11:55:54 +0100

    Fix search form button.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 "use strict";
 
-const openMenuButton = document.querySelector('#openMenuButton');
+const openMenuButton = document.querySelector("#openMenuButton");
 const searchForm = document.querySelector("#navigationSearchForm")
+const searchFormButton = document.querySelector("#searchFormButton");
 
 openMenuButton.addEventListener("click", openMenu);
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  openSearchField(e);
+});
+// searchFormButton.addEventListener("click", openSearchField);
 
 function openMenu(event) {
   const headerEl = document.querySelector("#header");
   let isOpenMenuButtonActive = openMenuButton.getAttribute("data-is-active") === "true";
-  searchForm.setAttribute("data-is-active", !isOpenMenuButtonActive);
-  openMenuButton.setAttribute("data-is-active", !isOpenMenuButtonActive);
-  headerEl.setAttribute("data-is-active", !isOpenMenuButtonActive);
+  openSearchField();
+  openMenuButton.setAttribute("data-is-active", String(!isOpenMenuButtonActive));
+  headerEl.setAttribute("data-is-active", String(!isOpenMenuButtonActive));
+}
+
+function openSearchField(event) {
+  const isFormActive = searchForm.getAttribute("data-is-active") === "true";
+  if (isFormActive) {
+    searchForm.submit();
+  }else {
+    searchForm.setAttribute("data-is-active", String(!isFormActive));
+    // document.addEventListener("click", (e) => searchForm.setAttribute("data-is-active", "false"));
+  }
 }
